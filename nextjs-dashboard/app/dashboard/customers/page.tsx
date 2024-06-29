c41e97156f4799aae49aeffd32e0f9efdb64973b
Vlad Kroz <vladimirkroz@Vladimirs-MacBook-Pro.local>
2024-06-29 15:23:28 -0700

    Using streaming for loading slow components

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
+export default async function Page() {
+    console.log('Simulate 3 sec delay...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     return <p>Customers Page</p>;
 }
