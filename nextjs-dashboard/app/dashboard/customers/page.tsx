dda6ddeda8728d7aa161480a33e5759c8c44a286
Vlad Kroz <vladimirkroz@Vladimirs-MacBook-Pro.local>
2024-06-29 15:25:53 -0700

    Make page loading specific only for dashboard home page, by adding (overview) route group

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,6 +1,6 @@
 export default async function Page() {
-    console.log('Simulate 3 sec delay...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // console.log('Simulate 3 sec delay...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     return <p>Customers Page</p>;
 }
